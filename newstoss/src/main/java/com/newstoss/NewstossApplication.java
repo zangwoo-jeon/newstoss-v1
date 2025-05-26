fe3eedaee71f20c3f442acf29b62253319332891
gudwns1812 <gudwns1812@naver.com>
2025-05-26 12:54:41 +0900

    init

---
+++ newstoss/src/main/java/com/newstoss/NewstossApplication.java
@@ -0,0 +1,13 @@
+package com.newstoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewstossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewstossApplication.class, args);
+    }
+
+}
---
+++ newstoss/src/test/java/com/newstoss/NewstossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newstoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstossApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
