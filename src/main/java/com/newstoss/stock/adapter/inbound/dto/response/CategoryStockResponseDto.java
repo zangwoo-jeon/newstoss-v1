ae40fb8864868bae8befad64b68a3b16cc620ad7
gudwns1812 <gudwns1812@naver.com>
2025-05-30 09:32:02 +0900

    [FIX] 주식 카테고리 api 수정

--- src/main/java/com/newstoss/stock/adapter/inbound/dto/response/CategoryStockResponseDto.java
+++ src/main/java/com/newstoss/stock/adapter/inbound/dto/response/CategoryStockResponseDto.java
@@ -14,4 +14,16 @@ public class CategoryStockResponseDto {
 
     @Schema(description = "주식 코드")
     private String StockCode;
+
+    @Schema(description = "주식 현재가")
+    private String CurrentPrice;
+
+    @Schema(description = "주식 전일 대비")
+    private String ChangeRate;
+
+    @Schema(description = "주식 전일 대비 기호")
+    private String Sign;
+
+    @Schema(description = "주식 전일 대비 가격")
+    private String ChangeAmount;
 }
