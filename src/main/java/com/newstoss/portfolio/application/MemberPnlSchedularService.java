ab01761e47883815c9bf00396a697d5efe700177
gudwns1812 <gudwns1812@naver.com>
2025-06-05 18:34:47 +0900

    [FEAT]  포트폴리오 관련 api 개발

--- src/main/java/com/newstoss/portfolio/application/MemberPnlSchedularService.java
+++ src/main/java/com/newstoss/portfolio/application/MemberPnlSchedularService.java
@@ -1,9 +1,11 @@
 package com.newstoss.portfolio.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberPnlSchedularService {
 }
