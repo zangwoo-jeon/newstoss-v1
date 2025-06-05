d859b0492dcaeb32bda6223f11d4117fd38dad3a
parkstar12 <zzanggu12280@naver.com>
2025-06-05 17:10:55 +0900

    [FEAT] 뉴스 db저장 및 redis,sse설정

---
+++ src/main/java/com/newstoss/news2/domain/NewsEntity.java
@@ -0,0 +1,30 @@
+package com.newstoss.news2.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "news")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewsEntity {
+    @Id
+    private String newsId;
+    private LocalDateTime wdate;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String article;
+    private String url;
+    private String press;
+    private String image;
+}
