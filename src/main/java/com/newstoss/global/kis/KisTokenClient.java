b1736e8cce263ef00acf08ab802783b0f66667c2
gudwns1812 <gudwns1812@naver.com>
2025-06-07 15:39:14 +0900

    [FEAT] 토큰 properties 수정

--- src/main/java/com/newstoss/global/kis/KisTokenClient.java
+++ src/main/java/com/newstoss/global/kis/KisTokenClient.java
@@ -18,10 +18,10 @@ public class KisTokenClient {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         System.out.println("appkey: " + props.getAppkey());
-        System.out.println("grant_type: " + props.getGrant_Type());
+        System.out.println("grant_type: " + props.getGrantType());
         System.out.println("appsecret: " + props.getAppsecret());
         Map<String, String> body = Map.of(
-                "grant_type", props.getGrant_Type(),
+                "grant_type", props.getGrantType(),
                 "appkey", props.getAppkey(),
                 "appsecret", props.getAppsecret()
         );
@@ -29,7 +29,7 @@ public class KisTokenClient {
         HttpEntity<Map<String, String>> entity = new HttpEntity<>(body, headers);
 
         ResponseEntity<KisTokenResponse> response = restTemplate.exchange(
-                props.getToken_url(),
+                props.getTokenUrl(),
                 HttpMethod.POST,
                 entity,
                 KisTokenResponse.class
