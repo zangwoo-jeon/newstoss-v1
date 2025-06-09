5f799f1124fabb5cd1ac79b523ad2f54b4079ba0
parkstar12 <zzanggu12280@naver.com>
2025-06-09 18:09:43 +0900

    [FIX] 프론트 응답 수정

--- src/main/java/com/newstoss/news/adapter/in/web/sse/SseEmitters.java
+++ src/main/java/com/newstoss/news/adapter/in/web/sse/SseEmitters.java
@@ -1,5 +1,7 @@
 package com.newstoss.news.adapter.in.web.sse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -10,6 +12,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
 @Component
 public class SseEmitters {
     private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
+    @Autowired
+    private ObjectMapper objectMapper;
 
     public SseEmitter add() {
         SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
@@ -22,6 +26,12 @@ public class SseEmitters {
     }
 
     public void send(Object data) {
+        try {
+            System.out.println("직렬화된 결과: " + objectMapper.writeValueAsString(data));
+        } catch (Exception e) {
+            System.out.println("직렬화 실패: " + e.getMessage());
+        }
+
         emitters.forEach(emitter -> {
             try {
                 emitter.send(SseEmitter.event().name("news").data(data));
