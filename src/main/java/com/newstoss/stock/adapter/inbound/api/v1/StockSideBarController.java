03ed0e051caf395beae74739849eb33378ece9ad
gudwns1812 <gudwns1812@naver.com>
2025-06-11 15:09:37 +0900

    [FIX] FX api 수정

--- src/main/java/com/newstoss/stock/adapter/inbound/api/v1/StockSideBarController.java
+++ src/main/java/com/newstoss/stock/adapter/inbound/api/v1/StockSideBarController.java
@@ -3,6 +3,12 @@ package com.newstoss.stock.adapter.inbound.api.v1;
 import com.newstoss.global.response.SuccessResponse;
 import com.newstoss.stock.adapter.inbound.dto.response.FxResponseDto;
 import com.newstoss.stock.application.port.in.GetFxInfoUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +27,25 @@ public class StockSideBarController {
 
     private final GetFxInfoUseCase getFxInfoUseCase;
 
+    @Operation(
+            summary = "FX, 원자재, 채권 조회",
+            description = "FX, 원자재, 채권 조회합니다. type 에 종류를 적고, symbol에 조회할 종목이름을 적습니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "카테고리 조회 성공",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = FxResponseDto.class)
+                            )
+                    )
+            }
+    )
+    @Parameters({
+            @Parameter(name = "type", description = "조회할 주식 타입", example = "FX , Feed, Bonds"),
+            @Parameter(name = "symbol", description = "조회할 코드 이름", example = "FX: dollar, yen, DOW, NQ, SP500, Nikkei, Hangsang"
+            + ", ShangHai , Feed: GOLD, SILVER, WTI, CORN, COFFEE ,COTTON , Bonds: KRBONDS 1,3,5,10,20,30 USBONDS 1,10,30")
+    })
     @GetMapping("/FX")
     public ResponseEntity<?> getFX(@RequestParam String type,
                                    @RequestParam String symbol) {
