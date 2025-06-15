0697fa2555bc784f9ca9d03ab3d2191b26ce2d6b
parkstar12 <zzanggu12280@naver.com>
2025-06-15 19:38:14 +0900

    Update PortfolioStockQueryServiceTest.java

--- src/test/java/com/newstoss/portfolio/application/PortfolioStockQueryServiceTest.java
+++ src/test/java/com/newstoss/portfolio/application/PortfolioStockQueryServiceTest.java
@@ -1,44 +1,65 @@
-//package com.newstoss.portfolio.application;
-//
-//import com.newstoss.portfolio.adapter.inbound.web.dto.response.PortfolioDailyPnlResponseDto;
-//import com.newstoss.stock.adapter.outbound.persistence.repository.StockRepository;
-//import com.newstoss.stock.entity.Stock;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.UUID;
-//
-//import static org.assertj.core.api.Assertions.*;
-//
-//@SpringBootTest
-//@Transactional
-//class PortfolioStockQueryServiceTest {
-//
-//    @Autowired
-//    private PortfolioQueryService service;
-//
-//    @Autowired
-//    private PortfolioStockCommandService commandService;
-//
-//    @Autowired
-//    private StockRepository stockRepository;
-//
-//
-//    @Test
-//    public void portfoliosTest() {
-//        //given
-//        UUID id = UUID.randomUUID();
-//        Stock stock1 = stockRepository.findByName("삼성전자").get();
-//        Stock stock2 = stockRepository.findByName("카카오").get();
-//        commandService.createPortfolioStock(id, stock1.getStockCode(), 10, 60000);
-//        commandService.createPortfolioStock(id, stock2.getStockCode(), 5, 100000);
-//        //when
-//        PortfolioDailyPnlResponseDto portfolioStocks = service.getPortfolioStocks(id);
-//
-//        //then
-//        assertThat(portfolioStocks).isNotNull();
-//        System.out.println("portfolioStocks.getTotalPnl() = " + portfolioStocks.getTotalPnl());
-//    }
-//}
+package com.newstoss.portfolio.application;
+
+import com.newstoss.member.adapter.in.web.dto.requestDTO.AddressDTO;
+import com.newstoss.member.adapter.in.web.dto.requestDTO.SignupRequestDTO;
+import com.newstoss.member.adapter.out.persistence.JpaMemberRepository;
+import com.newstoss.member.application.in.command.SignupService;
+import com.newstoss.member.domain.Member;
+import com.newstoss.portfolio.adapter.inbound.web.dto.response.PortfolioDailyPnlResponseDto;
+import com.newstoss.portfolio.adapter.outbound.persistence.repository.JpaPortfolioRepository;
+import com.newstoss.portfolio.entity.Portfolio;
+import com.newstoss.stock.adapter.outbound.persistence.repository.StockRepository;
+import com.newstoss.stock.entity.Stock;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class PortfolioStockQueryServiceTest {
+
+    @Autowired
+    private PortfolioQueryService service;
+
+    @Autowired
+    private PortfolioStockCommandService commandService;
+
+    @Autowired
+    private PortfolioService portfolioService;
+
+    @Autowired
+    private StockRepository stockRepository;
+    @Autowired
+    private JpaMemberRepository memberRepository;
+    @Autowired
+    private JpaPortfolioRepository portfolioRepository;
+    @Autowired
+    private EntityManager em;
+    @Autowired
+    private SignupService signupService;
+
+    @Test
+    public void portfoliosTest() {
+        //given
+        UUID id = UUID.randomUUID();
+        Stock stock1 = stockRepository.findByName("삼성전자").get();
+        Stock stock2 = stockRepository.findByName("카카오").get();
+        commandService.createPortfolioStock(id, stock1.getStockCode(), 10, 60000);
+        commandService.createPortfolioStock(id, stock2.getStockCode(), 5, 100000);
+        //when
+        PortfolioDailyPnlResponseDto portfolioStocks = service.getPortfolioStocks(id);
+
+        //then
+        assertThat(portfolioStocks).isNotNull();
+    }
+
+
+
+}
