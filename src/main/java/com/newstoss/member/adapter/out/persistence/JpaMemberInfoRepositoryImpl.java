828b8997f31aa3cbd8a0613444dba11a1ad9fe26
gudwns1812 <gudwns1812@naver.com>
2025-06-16 14:26:56 +0900

    [FEAT] ml api 생성

--- src/main/java/com/newstoss/member/adapter/out/persistence/JpaMemberInfoRepositoryImpl.java
+++ src/main/java/com/newstoss/member/adapter/out/persistence/JpaMemberInfoRepositoryImpl.java
@@ -5,14 +5,18 @@ import com.newstoss.member.adapter.in.web.dto.response.MemberStockDto;
 import com.newstoss.member.adapter.in.web.dto.response.QMemberInfoDto;
 
 import com.newstoss.member.adapter.in.web.dto.response.QMemberStockDto;
+import com.newstoss.portfolio.entity.QMemberPnl;
 import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 import static com.newstoss.member.domain.QMember.*;
+import static com.newstoss.portfolio.entity.QMemberPnl.memberPnl;
 import static com.newstoss.portfolio.entity.QPortfolio.*;
 import static com.newstoss.portfolio.entity.QPortfolioStock.*;
 
@@ -26,21 +30,22 @@ public class JpaMemberInfoRepositoryImpl implements JpaMemberInfoRepository {
     public MemberInfoDto findMemberInfo(UUID id) {
         MemberInfoDto dto = new MemberInfoDto();
         Tuple result = queryFactory
-                .select(member.name, portfolio.asset)
+                .select(member.name, memberPnl.asset)
                 .from(member)
-                .join(portfolio).on(portfolio.memberId.eq(member.memberId))
-                .where(member.memberId.eq(id))
+                .join(memberPnl).on(memberPnl.memberId.eq(member.memberId))
+                .where(member.memberId.eq(id), memberPnl.date.eq(LocalDate.now()))
                 .fetchOne();
 
         if (result == null) return null;
 
         dto.setUsername(result.get(member.name));
-        dto.setAsset(result.get(portfolio.asset));
+        dto.setAsset(result.get(memberPnl.asset));
 
         List<MemberStockDto> dtos = queryFactory
                 .select(new QMemberStockDto(portfolioStock.stock.stockCode, portfolioStock.stock.name ,
                         portfolioStock.unrealizedPnl))
                 .from(portfolioStock)
+                .where(portfolioStock.memberId.eq(id))
                 .fetch();
         long pnl = 0L;
         for (MemberStockDto memberStockDto : dtos) {
