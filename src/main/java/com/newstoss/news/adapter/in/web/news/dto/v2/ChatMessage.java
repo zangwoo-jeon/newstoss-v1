372e59af71741ee576723a779864dd2d8bd605b0
parkstar12 <zzanggu12280@naver.com>
2025-06-16 17:29:41 +0900

    [FIX] dto Key 값 성정

---
+++ src/main/java/com/newstoss/news/adapter/in/web/news/dto/v2/ChatMessage.java
@@ -0,0 +1,12 @@
+package com.newstoss.news.adapter.in.web.news.dto.v2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+    String message;
+}
