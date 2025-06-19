8e44d501eee1896234638095b261e8126765ec4d
zangwoo Jeon <create7827@naver.com>
2025-06-19 16:42:05 +0900

    fix : 원상태 복귀

--- src/main/java/com/newstoss/news/application/news/v2/port/in/GetNewsDetailUseCaseV2.java
+++ src/main/java/com/newstoss/news/application/news/v2/port/in/GetNewsDetailUseCaseV2.java
@@ -1,10 +1,7 @@
 package com.newstoss.news.application.news.v2.port.in;
 
-import com.newstoss.member.domain.Member;
 import com.newstoss.news.adapter.in.web.news.dto.v2.NewsDTOv2;
 
-import java.util.UUID;
-
 public interface GetNewsDetailUseCaseV2 {
-    NewsDTOv2 exec(String newsId, UUID member);
+    NewsDTOv2 exec(String newsId);
 }
