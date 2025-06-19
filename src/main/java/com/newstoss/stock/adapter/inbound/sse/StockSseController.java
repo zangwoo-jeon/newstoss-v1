cc346ff7fd61f4ae6b419d71797f1c8e74f9c2f8
gudwns1812 <gudwns1812@naver.com>
2025-06-19 20:03:12 +0900

    [FIX] sse 생성

--- src/main/java/com/newstoss/stock/adapter/inbound/sse/StockSseController.java
+++ src/main/java/com/newstoss/stock/adapter/inbound/sse/StockSseController.java
@@ -5,15 +5,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 import java.util.UUID;
 
+@CrossOrigin(origins = "https://news-toss.vercel.app")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v2/stock/sse")
