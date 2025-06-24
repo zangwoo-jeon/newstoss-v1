d3b0782b8aa0d1d0fd47a80561f0285aa80d4c64
zangwoo Jeon <create7827@naver.com>
2025-06-24 15:13:53 +0900

    fix : cd 재시도

--- src/main/java/com/newstoss/news/adapter/in/web/scrap/dto/ExternalNewsDTO.java
+++ src/main/java/com/newstoss/news/adapter/in/web/scrap/dto/ExternalNewsDTO.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ExternalNewsDTO {
-    //d
     private String news_id;
     private String wdate;
     private String title;
