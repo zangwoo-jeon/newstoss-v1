55d6bbc1a905d4326cdd833af235b7ecd68351c2
parkstar12 <zzanggu12280@naver.com>
2025-06-25 13:10:27 +0900

    [FIX] 빈 이름 중복 제거

--- src/main/java/com/newstoss/news/application/redis/port/out/RealTimeNewsCachePort.java
+++ src/main/java/com/newstoss/news/application/redis/port/out/RealTimeNewsCachePort.java
@@ -1,13 +1,13 @@
-package com.newstoss.news.application.redis.port.out;
-
-import com.newstoss.news.adapter.in.web.news.dto.v2.HighlightNewsDTO;
-import com.newstoss.news.adapter.in.web.news.dto.v2.NewsMathRelatedDTO;
-import com.newstoss.news.adapter.in.web.news.dto.v2.RealTimeNewsDTO;
-
-import java.util.List;
-
-public interface RealTimeNewsCachePort {
-    void saveRealTimeNews(RealTimeNewsDTO recentNews);
-    List<RealTimeNewsDTO> loadRecentNews();
-}
-
+//package com.newstoss.news.application.redis.port.out;
+//
+//import com.newstoss.news.adapter.in.web.news.dto.v2.HighlightNewsDTO;
+//import com.newstoss.news.adapter.in.web.news.dto.v2.NewsMathRelatedDTO;
+//import com.newstoss.news.adapter.in.web.news.dto.v2.RealTimeNewsDTO;
+//
+//import java.util.List;
+//
+//public interface RealTimeNewsCachePort {
+//    void saveRealTimeNews(RealTimeNewsDTO recentNews);
+//    List<RealTimeNewsDTO> loadRecentNews();
+//}
+//
